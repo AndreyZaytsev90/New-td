b41bd81767c5aac06ee6d6d2ba3bc98449f370c6
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-03-18 20:33:00 +0300

    Added new component

---
+++ new-td/src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type PropsButtonType = {
+    name: string
+    callback: () => void
+}
+
+export const Button = (props: PropsButtonType) => {
+
+    const onClickHandler = () => {
+        props.callback()
+    }
+
+    return (
+        <button onClick={onClickHandler}>{props.name}</button>
+    );
+};
